53eafbdcf116796f4a1443c8c03bff1dbe6e71e0
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-05-06 15:26:12 +0200

    Users can now send friend requests, and get the requests they have recieved

--- BlogAPI/Models/BlogContext.cs
+++ BlogAPI/Models/BlogContext.cs
@@ -39,10 +39,14 @@ namespace BlogAPI.Models
 			modelBuilder.Entity<User>()
 				.HasMany(e => e.Friends)
 				.WithMany();
-		}
+
+            modelBuilder.Entity<FriendRequest>()
+				.HasKey(fr => new { fr.SenderId, fr.RecipientId });
+        }
 
 		public DbSet<User> Users { get; set; }
 		public DbSet<Post> Posts { get; set; }
 		public DbSet<Comment> Comments { get; set; }
+		public DbSet<FriendRequest> FriendRequests { get; set; }
 	}
 }
---
+++ BlogAPI/Models/FriendRequest.cs
@@ -0,0 +1,8 @@
+namespace BlogAPI.Models
+{
+    public class FriendRequest
+    {
+        public int SenderId { get; set; }
+        public int RecipientId { get; set; }
+    }
+}
