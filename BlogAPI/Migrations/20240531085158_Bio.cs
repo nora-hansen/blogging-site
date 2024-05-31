561bb71862326a26ce14df91f6f68cfeb97ded7d
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-05-31 10:57:26 +0200

    Added bio

---
+++ BlogAPI/Migrations/20240531085158_Bio.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Bio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "Profiles",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "Profiles");
+        }
+    }
+}
--- BlogAPI/Models/Profile.cs
+++ BlogAPI/Models/Profile.cs
@@ -9,7 +9,8 @@ namespace BlogAPI.Models
 		public string? bgColor { get; set; } = null;
 		public string? fontColor { get; set; } = null;
 		public string? postColor { get; set; } = null;
-		public DateTime CreatedAt { get; set; }
+        public string? Bio { get; set; } = null;
+        public DateTime CreatedAt { get; set; }
 		public DateTime LastUpdated { get; set; }
 
 		public Profile()
